1b0e0e23fcc84fabedd32926b77b3ca227e11a99
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-09-08 22:11:48 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,26 +1,26 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def rectangle_area(length, width):
+def last_digit_even_or_odd(length, width):
     # Write your code here
     pass
 
 # Exercise 2
-def check_number(num):
+def check_positive_negative(num):
     # Write your code here
     pass
 
 # Exercise 3
-def is_divisible_by_5(num):
+def has_even_digits(num):
     # Write your code here
     pass
 
 # Exercise 4
-def sum_is_even_or_odd(a, b, c, d):
+def largest_digit(a, b, c, d):
     # Write your code here
     pass
 
 # Exercise 5
-def divisible_by_sum_of_digits(num):
+def second_last_digit_is_5(num):
     # Write your code here
     pass
